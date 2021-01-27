fb000077e0a974a17adab129fbc121c9409bd90e
Shangfeng Liu <codershangfeng@gmail.com>
2021-01-27 09:09:27 +0800

    Retrieve vote options from backend server.

--- src/component/Vote.js
+++ src/component/Vote.js
@@ -1,23 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Radio, FormControl, RadioGroup, FormControlLabel } from '@material-ui/core';
 
 function Vote() {
-    const [options, setOptions] = useState(['apple', 'pear'])
+    const [options, setOptions] = useState([])
+    const [error, setError] = useState(null)
+    const [isLoaded, setIsLoaded] = useState(false)
 
-    const CreateOption = function (option) {
-        return <FormControlLabel value={option} control={<Radio />} label={option} />
+    useEffect(() => {
+        fetch("http://localhost:8080")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true)
+                    setOptions(result)
+                },
+                (err) => {
+                    setIsLoaded(true)
+                    setError(err)
+                }
+            )
+    })
 
-    }
-
-    return (
-        <div>
+    if (error) {
+        return <div>Ops, Failed!</div>
+    } else if (!isLoaded) {
+        return <div>Waiting, still loading...</div>
+    } else {
+        return <div>
             <FormControl component="fieldset" >
                 <RadioGroup aria-label="quiz" name="quiz">
-                    {options.map(CreateOption)}
+                    {options.map((option) => <FormControlLabel value={option} control={<Radio />} label={option} />)}
                 </RadioGroup>
             </FormControl>
         </div>
-    )
+    }
 }
 
 export default Vote;
